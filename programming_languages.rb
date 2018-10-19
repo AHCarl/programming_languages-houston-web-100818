a5a1291d4a9c16a962a737f4bcf9845e00223508
Alexander Carlin <ahcarlin@gmail.com>
2018-10-19 19:28:13 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,9 +1,12 @@
+require 'pry'
+
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |paradigm, lang|
     new_hash[paradigm] = lang 
     lang.each do |type, val|
       new_hash[lang] = type 
+      
       new_hash[lang][type].values = val
     end 
   end 
