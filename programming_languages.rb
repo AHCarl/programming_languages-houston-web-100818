d45c65c008057048ff237430afab64be3afc7f29
Alexander Carlin <ahcarlin@gmail.com>
2018-10-19 19:45:30 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,11 +3,11 @@ require 'pry'
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |paradigm, lang|
-    new_hash[lang] = {
     lang.each do |type, val|
       new_hash[lang] = type 
       new_hash[lang] = style
-      new_hash[lang][type].value = val
+      new_hash[lang][type] = paradigm
+      new_hash[lang][style]
     end 
   end 
   new_hash
